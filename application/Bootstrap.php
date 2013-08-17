5a39e3124edbbc8cbd70f8d2fadb0b5cbd8afdfb
Josh <josh@joshuacullen.com>
2013-08-16 23:56:36 -0700

    Added grunt with Jquery and Sass

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+	protected function _initDoctype()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->metaDescription = 'Pacifika Consultants Ltd are positive change management professionals. We specialise in Stakeholder and Community Engagement with Indigenous cultures in the Asia Pacific region';
+	}
+	
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,6 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+    	$this->view->headTitle('Pacifika Consultants');
         // action body
     }
 
