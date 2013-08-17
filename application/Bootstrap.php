2b2943f8a389c5e2b8023d1541c9d95c7892883c
Josh <josh@joshuacullen.com>
2013-08-17 00:02:36 -0700

    Moved meta description to controller

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,7 +6,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
 		$this->bootstrap('view');
 		$view = $this->getResource('view');
-		$view->metaDescription = 'Pacifika Consultants Ltd are positive change management professionals. We specialise in Stakeholder and Community Engagement with Indigenous cultures in the Asia Pacific region';
+		$view->metaDescription = '';
 	}
 	
 }
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,15 +5,14 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->view->headTitle('Pacifika Consultants');
+    	$this->view->metaDescription = 'Pacifika Consultants Ltd are positive change management professionals. We specialise in Stakeholder and Community Engagement with Indigenous cultures in the Asia Pacific region';
     }
 
     public function indexAction()
     {
-    	$this->view->headTitle('Pacifika Consultants');
-        // action body
-    }
 
+    }
 
 }
 
