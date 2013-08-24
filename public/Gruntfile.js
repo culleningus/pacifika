66ae27623a61c066448fa6dc46f5093974cb4408
Josh <josh@joshuacullen.com>
2013-08-23 20:34:33 -0700

    Adding grunt-contrib-watch

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -7,9 +7,19 @@ module.exports = function(grunt){
                     'css/screen.css': 'sass/screen.scss' 
                 },
             }
-        }
+        },
+        watch: {
+            scripts: {
+                files: '**/*.scss',
+                tasks: ['sass'],
+                options: {
+                    interrupt: true,
+                },
+            },
+        }                
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', 'sass');
 }
