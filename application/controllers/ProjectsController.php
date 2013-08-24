722cae9870e5e5f26cb0f910a28906738d0a45d5
Josh <josh@joshuacullen.com>
2013-08-23 21:56:34 -0700

    Adding structure to the db

--- application/controllers/ProjectsController.php
+++ application/controllers/ProjectsController.php
@@ -5,7 +5,7 @@ class ProjectsController extends Zend_Controller_Action
 
     public function init()
     {
-        $this->view->headTitle('Pacifika Consultants Projects');
+        $this->view->headTitle('Pacifika Consultants - Projects');
     	$this->view->metaDescription = 'Read about Pacifika Consultants previous projects';
     }
 
